94787e81f913be403827c46f0a06a2697954023c
Brandon Taylor <brandon.taylor221@gmail.com>
2018-02-26 16:36:01 -0500

    updates

--- src/Keys.jl
+++ src/Keys.jl
@@ -1,44 +1,48 @@
 module Keys
 
-using TypedBools
-using RecurUnroll
-
-struct Key{K} end
+import MacroTools
+import RecurUnroll
+import RecurUnroll: getindex_unrolled
+import TypedBools
 
 export Key
-"""
-    Key(k)
+struct Key{K} end
 
-A key for indexing tuples
-"""
-Base.@pure Key(k) = Key{k}()
+Key(k) = Key{k}()
 
 function Base.show(io::IO, k::Key{K}) where K
     print(io, ".")
     print(io, K)
 end
 
-struct Keyed{K, V}
-    value::V
-end
+const Keyed{K} = Tuple{Key{K}, V} where {K, V}
 
+export key
+key(k::Keyed) = k[1]
+export value
+value(k::Keyed) = k[2]
+value(any) = any
+
+export @keyed_tuple
 """
-    Keyed(key, value)
+    @keyed_tuple args...
 
-If tuples contain Keyed values, you can index them with [`Keys`](@ref) as if
-they were a Dict.
+will create a regular tuple. If args contains assignments, then it will return
+Keyed values. You can index them with [`Keys`](@ref) as if they were a Dict. On
+0.7, you can also access values with `.`. Duplicated keys are allowed; will
+return the first match.
 
 ```jldoctest
 julia> using Keys, TypedBools, Base.Test
 
-julia> k = (Keyed(:a, 1), 2.0, Keyed(:c, "c"))
-(a = 1, 2.0, c = "c")
+julia> k = @keyed_tuple a = 1 2.0 c = "c"
+((.a, 1), 2.0, (.c, "c"))
 
 julia> @inferred k[Key(:c)]
 "c"
 
 julia> k[Key(:d)]
-ERROR: Key .c not found
+ERROR: Key .d not found
 [...]
 
 julia> @inferred haskey(k, Key(:c))
@@ -46,36 +50,41 @@ TypedBools.True()
 
 julia> @inferred Base.setindex(k, 3//1, Key(:c))
 (a = 1, 2.0, c = 3//1)
+
+julia> if VERSION > v"0.6.2"
+            @inferred (k -> k.c)(k)
+        else
+            "c"
+        end
+"c"
 ```
 """
-Base.@pure Keyed(key, value::V) where V = Keyed{key, V}(value)
-
-function Base.show(io::IO, k::Keyed{K}) where K
-    print(io, K)
-    print(io, " = ")
-    show(io, k.value)
+macro keyed_tuple(args...)
+    Expr(:tuple, map(args) do item
+        MacroTools.@match item begin
+            (a_ = b_) => :(($(Key(a)), $b))
+            any_ => any
+        end
+    end...) |> esc
 end
-KeyedTuple(; args...) = KeyedTuple(args)
 
-value(k::Keyed) = k.value
+match_key(::Keyed{K}, ::Key{K}) where K = TypedBools.True()
+match_key(any, ::Key) = TypedBools.False()
 
-match_key(::Keyed{K}, ::Key{K}) where K = True()
-match_key(any, ::Key) = False()
-
-first_error(::Tuple{}, k::Key) = error("Key $k not found")
-first_error(t::Tuple, k::Key) = value(first(t))
+head_error(::Tuple{}, k::Key) = error("Key $k not found")
+head_error(t::Tuple, k::Key) = value(RecurUnroll.head(t))
 
 which_key(t::Tuple, k::Key) = map(t) do keyed
     match_key(keyed, k)
 end
 
 Base.getindex(t::Tuple, k::Key) =
-    first_error(getindex_unrolled(t, which_key(t, k)), k)
-Base.haskey(t::Tuple, k::Key) = reduce_unrolled(|, which_key(t, k))
+    head_error(getindex_unrolled(t, which_key(t, k)), k)
+Base.haskey(t::Tuple, k::Key) = RecurUnroll.reduce_unrolled(|, which_key(t, k))
 
-Base.setindex(t::Tuple, value::V, k::Key{K}) where {K, V} =
+Base.setindex(t::Tuple, avalue, k::Key) =
     map(t) do keyed
-        ifelse(match_key(keyed, k), Keyed{K, V}(value), keyed)
+        ifelse(match_key(keyed, k), (k, avalue), keyed)
     end
 
 export delete
@@ -87,10 +96,19 @@ Delete all values matching k
 ```jldoctest
 julia> using Keys, Base.Test
 
-julia> @inferred delete((Keyed(:a, 1), Keyed(:b, 2.0)), Key(:b))
-(b = 2,)
+julia> @inferred delete((@keyed_tuple a = 1 b = 2.0), Key(:b))
+((.a, 1),)
 ```
 """
 delete(t::Tuple, k::Key) = getindex_unrolled(t, map(!, which_key(t, k)))
+
+if VERSION > v"0.6.2"
+    keyed_tuple(n::NamedTuple) = map(keys(x)) do key
+        (Key(x), getproperty(x, key))
+    end
+    # Potentially serious type piracy. However, absolutely necessary to get full
+    # covariance. Will error if no Keys are used.
+    Base.getproperty(t::Tuple, s::Symbol) = getindex(t, Key(s))
+end
 
 end
