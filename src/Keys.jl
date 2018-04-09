ea780bfc5725cc4b44a6bf8c33cc29e7fd10a54f
Brandon Taylor <brandon.taylor221@gmail.com>
2018-04-09 14:12:53 -0400

    changes

--- src/Keys.jl
+++ src/Keys.jl
@@ -2,7 +2,7 @@ module Keys
 
 import RecurUnroll: getindex_unrolled, reduce_unrolled
 import TypedBools: True, False, not
-import Base: getindex, haskey, merge
+import Base: getindex, haskey, merge, tail
 import MacroTools: @match
 import Base.Meta: quot
 
@@ -10,56 +10,69 @@ export Key
 """
     struct Key{K}
 
-A typed key.
-
-```jldoctest
-julia> using Keys
-
-julia> Key(:a)
-.a
+A typed key
 ```
 """
 struct Key{K} end
 
-const Some{T} = NTuple{N, T} where N
-
-const SomeKeys = Some{Key}
-const KeyOrKeys = Union{Key, SomeKeys}
-
 @inline Key(s::Symbol) = Key{s}()
 
-const SymbolOrSymbols = Union{Symbol, Some{Symbol}}
-
-@inline to_keys(s::Symbol) = Key(s)
-@inline to_keys(ss::Some{Symbol}) = map(Key, ss)
+const SomeKeys = NTuple{N, Key} where N
 
 function Base.show(io::IO, key::Key{K}) where K
-    print(io, ".")
+    print(io, :.)
     print(io, K)
+end
+
+export @k_str
+"""
+    k_str(s::String)
+
+Make a key
+
+```jldoctest
+julia> using Keys
+
+julia> k"a"
+.a
+```
+"""
+macro k_str(s::String)
+    esc(:($Key{$(quot(Symbol(s)))}()))
+end
+
+replace_keys(anything) = anything
+replace_keys(q::QuoteNode) = replace_keys(quot(q.value))
+replace_keys(e::Expr) = @match e begin
+    a_.b_ => :($(replace_keys(a)).$b)
+    :(a_) => :($Key{$(quot(a))}())
+    e_ => Expr(e.head, map(replace_keys, e.args)...)
+end
+
+export @keys
+"""
+    macro keys(e)
+
+Make any symbol in e a key.
+
+```jldoctest
+julia> using Keys
+
+julia> @keys (:a => 1, :b => 2)
+(.a=>1, .b=>2)
+```
+"""
+macro keys(e)
+    esc(replace_keys(e))
 end
 
 export Keyed
 """
     struct Keyed{K, V}
 
-A keyed value.
-
-```jldoctest
-julia> using Keys
-
-julia> Keyed{:a}(1)
-a = 1
-
-julia> Keyed(Key(:a), 1)
-a = 1
-```
+An alias for a key-value pair.
 """
-struct Keyed{K, V}
-    value::V
-end
-
-Keyed{K}(v::V) where {K, V} = Keyed{K, V}(v)
-Keyed(k::Key{K}, value::V) where {K, V} = Keyed{K, V}(value)
+const Keyed{K, V} = Pair{Key{K}, V} where {K, V}
 
 export key
 """
@@ -70,11 +83,11 @@ Get the key of a [`Keyed`](@ref) value.
 ```jldoctest
 julia> using Keys
 
-julia> map(key, @keyed_tuple(a = 1, b = 2.0))
+julia> @keys key.((:a => 1, :b => 2.0))
 (.a, .b)
 ```
 """
-key(k::Keyed{K}) where K = Key{K}()
+key(::Keyed{K}) where K = Key{K}()
 
 export value
 """
@@ -85,17 +98,11 @@ Get the value of a [`Keyed`](@ref) value.
 ```jldoctest
 julia> using Keys
 
-julia> map(value, @keyed_tuple(a = 1, b = 1.0))
+julia> @keys value.((:a => 1, :b => 1.0))
 (1, 1.0)
 ```
 """
-value(k::Keyed) = k.value
-
-function Base.show(io::IO, k::Keyed{K}) where K
-    print(io, K)
-    print(io, " = ")
-    show(io, value(k))
-end
+value(a_keyed_tuple::Keyed) = a_keyed_tuple.second
 
 export KeyedTuple
 """
@@ -103,28 +110,11 @@ export KeyedTuple
 
 A tuple with only [`Keyed`](@ref) values.
 """
-const KeyedTuple = Union{
-    NTuple{1, Keyed},
-    NTuple{2, Keyed},
-    NTuple{3, Keyed},
-    NTuple{4, Keyed},
-    NTuple{5, Keyed},
-    NTuple{6, Keyed},
-    NTuple{7, Keyed},
-    NTuple{8, Keyed},
-    NTuple{9, Keyed},
-    NTuple{10, Keyed},
-    NTuple{11, Keyed},
-    NTuple{12, Keyed},
-    NTuple{13, Keyed},
-    NTuple{14, Keyed},
-    NTuple{15, Keyed},
-    NTuple{16, Keyed}
-}
+const KeyedTuple = Tuple{Keyed, Vararg{Keyed}}
 
-export @keyed_tuple
+export @keyed
 """
-    @keyed_tuple(args...)
+    @keyed(args...)
 
 Construct a [`KeyedTuple`](@ref). You can index them with symbols as
 if they were a Dict. On 0.7, you can also access values with `.`. Duplicated
@@ -133,41 +123,35 @@ keys are allowed; will return the first match.
 ```jldoctest
 julia> using Keys
 
-julia> k = @keyed_tuple(a = 1, b = 1.0)
-(a = 1, b = 1.0)
+julia> @keys a_keyed_tuple = (:a => 1, :b => 1.0)
+(.a=>1, .b=>1.0)
 
 julia> if VERSION >= v"0.7.0-DEV"
-            k.b
+            a_keyed_tuple.b
         else
-            k[:b]
+            @keys a_keyed_tuple[:b]
         end
 1.0
 
-julia> getindex(k, (:a, :b))
-(a = 1, b = 1.0)
+julia> @keys a_keyed_tuple[(:a, :b)]
+(.a=>1, .b=>1.0)
 
-julia> k[:c]
+julia> @keys a_keyed_tuple[:c]
 ERROR: Key .c not found
 [...]
 
-julia> haskey(k, :b)
+julia> @keys haskey(a_keyed_tuple, :b)
 TypedBools.True()
-
-julia> merge(@keyed_tuple(a = 1, b = 1.0), @keyed_tuple(b = "a", c = 1 // 1))
-(a = 1, b = "a", c = 1//1)
-
-julia> @keyed_tuple(1)
-ERROR: All arguments to keyed_tuple must be an assignments
-[...]
 ```
 """
-macro keyed_tuple(args...)
-    esc(:($(map(args) do arg
+macro keyed(e::Expr)
+    map!(e.args) do arg
         @match arg begin
             (akey_ = avalue_) => :($Keyed{$(quot(akey))}($avalue))
-            any_ => error("All arguments to keyed_tuple must be an assignments")
+            any_ => any
         end
-    end...),))
+    end
+    esc(e)
 end
 
 match_key(::Keyed{K}, ::Key{K}) where K = True()
@@ -185,7 +169,7 @@ end
 first_error(::Tuple{}, key::Key) = error("Key $key not found")
 first_error(a_keyed_tuple::KeyedTuple, key::Key) = value(first(a_keyed_tuple))
 
-which_key(a_keyed_tuple::KeyedTuple, key::KeyOrKeys) = map(
+which_key(a_keyed_tuple::KeyedTuple, key::Union{Key, SomeKeys}) = map(
     let key = key
         keyed -> match_key(keyed, key)
     end,
@@ -201,42 +185,41 @@ getindex(a_keyed_tuple::KeyedTuple, key::Key) =
 getindex(a_keyed_tuple::KeyedTuple, keys::SomeKeys) =
     _getindex(a_keyed_tuple, keys)
 
-@inline getindex(a_keyed_tuple::KeyedTuple, ss::SymbolOrSymbols) =
-    getindex(a_keyed_tuple, to_keys(ss))
-
 haskey(a_keyed_tuple::KeyedTuple, key::Key) =
     reduce_unrolled(|, which_key(a_keyed_tuple, key))
 
-@inline haskey(a_keyed_tuple::KeyedTuple, s::Symbol) =
-    haskey(a_keyed_tuple, Key(s))
-
 export delete
 """
-    delete(key::KeyedTuple, key::Key)
+    delete(a_keyed_tuple::KeyedTuple, keys::Key...)
 
-Delete all values matching key
+Delete all keyed values matching keys.
 
 ```jldoctest
 julia> using Keys
 
-julia> delete(@keyed_tuple(a = 1, b = 2.0), :b)
-(a = 1,)
-
-julia> delete(@keyed_tuple(a = 1, b = 2.0), (:a, :b))
-()
+julia> @keys delete((:a => 1, :b => 1.0), :a)
+(.b=>1.0,)
 ```
 """
-delete(a_keyed_tuple::KeyedTuple, keys::KeyOrKeys) =
+delete(a_keyed_tuple::KeyedTuple, keys::Key...) =
     getindex_unrolled(a_keyed_tuple, map(
         not,
         which_key(a_keyed_tuple, keys)))
 
-@inline delete(a_keyed_tuple::KeyedTuple, ss::SymbolOrSymbols) =
-    delete(a_keyed_tuple, to_keys(ss))
+export push
+"""
+    push(k1::KeyedTuple, args...)
 
-function merge(k1::KeyedTuple, k2::KeyedTuple)
-    delete(k1, key.(k2))..., k2...
-end
+Push the pairs in args into `k1`, replacing common keys.
+
+```jldoctest
+julia> using Keys
+
+julia> @keys push((:a => 1, :b => 1.0), :b => "a", :c => 1 // 1)
+(.a=>1, .b=>"a", .c=>1//1)
+```
+"""
+push(k1::KeyedTuple, k2::Keyed...) = delete(k1, key.(k2)...)..., k2...
 
 if VERSION >= v"0.7.0-DEV"
     @inline Base.getproperty(key::KeyedTuple, s::Symbol) = getindex(key, Key(s))
@@ -251,15 +234,42 @@ Map f over the values of a keyed tuple.
 ```jldoctest
 julia> using Keys
 
-julia> map_values(x -> x + 1, @keyed_tuple(a = 1, b = 1.0))
-(a = 2, b = 2.0)
+julia> @keys map_values(x -> x + 1, (:a => 1, :b => 1.0))
+(.a=>2, .b=>2.0)
 ```
 """
-map_values(f, k::KeyedTuple) = map(
+map_values(f, a_keyed_tuple::KeyedTuple) = map(
     let f = f
-        keyed -> Keyed(key(keyed), f(value(keyed)))
+        keyed -> key(keyed) => f(value(keyed))
     end,
-    k
+    a_keyed_tuple
 )
+
+rename_one(replacement::Keyed{K2, Key{K1}}, old_keyed::Keyed{K1}) where {K1, K2} =
+    replacement.first => old_keyed.second
+rename_one(replacement::Keyed, old_keyed::Keyed) = old_keyed
+
+rename_single(replacement, ::Tuple{}) = ()
+rename_single(replacement, a_keyed_tuple) =
+    rename_one(replacement, first(a_keyed_tuple)),
+    rename_single(replacement, tail(a_keyed_tuple))...
+
+rename(a_keyed_tuple::KeyedTuple) = a_keyed_tuple
+
+"""
+    rename(a_keyed_tuple::KeyedTuple, replacements...)
+
+Replacements should be pairs of keys; where the first key matches in
+`a_keyed_tuple`, it will be replaced by the second.
+
+```
+julia> using Keys
+
+julia> @keys rename((:a => 1, :b => "a"), :c => :a)
+(.c=>1, .b=>"a")
+```
+"""
+rename(a_keyed_tuple::KeyedTuple, replacements...) =
+    rename(rename_single(first(replacements), a_keyed_tuple), tail(replacements)...)
 
 end
