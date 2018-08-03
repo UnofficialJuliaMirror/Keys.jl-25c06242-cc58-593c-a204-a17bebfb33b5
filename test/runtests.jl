9c90d1d0ad63c7ffc0507d6a1e1801a5bcbc33ba
bramtayl <brandon.taylor221@gmail.com>
2018-08-03 14:00:39 -0400

    require

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,23 @@ Documenter.makedocs(
     checkdocs = :exports,
     authors = "Brandon Taylor"
 )
+
+using Test
+
+x = (1, 2, 3)
+empty = ()
+switch = (True(), True(), False())
+new = (4, 5)
+
+@test_throws ArgumentError reduce_unrolled(+, empty)
+
+@test getindex_unrolled(x, empty) == empty
+@test getindex_unrolled(empty, switch) == empty
+
+@test setindex_many_unrolled(empty, empty, switch) == empty
+@test setindex_many_unrolled(empty, new, empty) == empty
+@test setindex_many_unrolled(empty, new, switch) == empty
+@test setindex_many_unrolled(x, empty, empty) == empty
+@test setindex_many_unrolled(x, empty, switch) === (missing, missing, 3)
+@test setindex_many_unrolled(x, new, empty) == empty
+@test setindex_many_unrolled(empty, empty, empty) == empty
