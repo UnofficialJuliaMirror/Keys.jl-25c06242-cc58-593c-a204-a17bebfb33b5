7b9815bd3c49cc51e272be919b9ff449f48cdc5f
Brandon Taylor <brandon.taylor221@gmail.com>
2018-09-16 23:03:52 -0400

    docs

--- src/Keys.jl
+++ src/Keys.jl
@@ -352,15 +352,15 @@ anonymize it (using the rules of `@_`) and also pass along a quoted version.
 julia> using Keys
 
 julia> @q 1
-Error: Expecting a function call
+Error: LoadError: Expecting a function call
 [...]
 
 julia> @q f(1)
-Error: Expecting function to end in a digit
+Error: LoadError: Expecting function to end in a digit
 [...]
 
 julia> @q f1()
-Error: Expecting at least 1 argument(s)
+Error: LoadError: Expecting at least 1 argument(s)
 [...]
 
 julia> call(source1, source2, anonymous, quoted) = anonymous(source1, source2);
