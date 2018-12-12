b5adabe78d107af8a49078974950ef4b8a5efd7b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-12 01:16:07 +0000

    build based on 3b9486b

--- versions.js
+++ versions.js
@@ -1,11 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
-  "latest",
-  "release-0.5",
-  "release-0.3",
-  "release-0.1",
-  "v0.5.0",
-  "v0.3.2",
-  "v0.3.1",
-  "v0.1.1",
+  "v0.5",
+  "v0.3",
+  "v0.1",
+  "dev",
 ];
